ce341cc46504b76ce698d84bcb08a2bdac235438
Edwin <edwinapol12@hotmail.com>
2020-09-13 19:44:55 -0500

    Updates

---
+++ precache-manifest.2ce428c25d1796d399c0f55da60d2cc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "badde3cd753a3fe29e14e321cee00780",
+    "url": "/React-pages/index.html"
+  },
+  {
+    "revision": "a652165115ab11de840d",
+    "url": "/React-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9a2e18a1a33927e370ce",
+    "url": "/React-pages/static/js/2.01327ab6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-pages/static/js/2.01327ab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a652165115ab11de840d",
+    "url": "/React-pages/static/js/main.299db970.chunk.js"
+  },
+  {
+    "revision": "78fa00353779605e0beb",
+    "url": "/React-pages/static/js/runtime-main.84742683.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
